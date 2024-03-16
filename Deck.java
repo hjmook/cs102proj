12ba72e0a869dc55d73d00a6ce3b4769f3da1509
LennelC <lennelchua@gmail.com>
2024-03-16 14:19:43 +0800

    Deck Class modifications

--- Deck.java
+++ Deck.java
@@ -1,22 +1,22 @@
 import java.util.*;
 
 public class Deck {
-    private ArrayList<Card> deckOfCards;
+    private ArrayList<Card> deck;
 
     public Deck() {
-        deckOfCards = new ArrayList<Card>();
-        String[] suits = {"Diamonds", "Hearts", "Clubs", "Spades"};
-        String[] values = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+        deck = new ArrayList<Card>();
+        String[] suits = {"D", "C", "H", "S"};
+        String[] values = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
         for (String suit : suits) {
             for (String value : values) {
                 Card card = new Card(suit, value);
-                deckOfCards.add(card);
+                deck.add(card);
             }
         }
     }
 
     public void displayDeck() {
-        for (Card card : deckOfCards) {
+        for (Card card : deck) {
             System.out.println(card);
         }
     }
@@ -24,31 +24,31 @@ public class Deck {
     public void shuffleDeck() {
         Random rand = new Random();
         // fisher-yates shuffle
-        for (int i = deckOfCards.size() - 1; i > 0; i--) {
+        for (int i = deck.size() - 1; i > 0; i--) {
             int j = rand.nextInt(i + 1);
-            Card temp = deckOfCards.get(i);
-            deckOfCards.set(i, deckOfCards.get(j));
-            deckOfCards.set(j, temp);
+            Card temp = deck.get(i);
+            deck.set(i, deck.get(j));
+            deck.set(j, temp);
         }
     }
 
     public int sizeOfDeck() {
-        return deckOfCards.size();
+        return deck.size();
     }
 
     public void returnCardToDeck(Card card) {
-        deckOfCards.add(card);
+        deck.add(card);
     }
 
     public Card drawTopCard() {
-        return deckOfCards.get(0);
+        return deck.get(0);
     }
 
     public Card drawRandomCard() {
         Random rand = new Random();
-        int randomIndex = rand.nextInt(deckOfCards.size());
+        int randomIndex = rand.nextInt(deck.size());
         // removes the drawn card from the deck as well
-        Card drawnCard = deckOfCards.remove(randomIndex);
+        Card drawnCard = deck.remove(randomIndex);
         return drawnCard;
     }
 }
